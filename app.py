f0d5cdfd1784a572e81b16c21ce430e82aa97376
Vincen <vincenhayate123@gmail.com>
2024-06-02 20:55:37 +0700

    Change interface

--- app.py
+++ app.py
@@ -15,34 +15,115 @@ def form():
 		col_1, col_2 = st.columns(2)
 
 		with col_1:
-			acc_balance = st.number_input('Account_Balance', value=0)
-			credit_monthly = st.number_input('Duration_of_Credit_monthly', value=0)
-			payment_status = st.number_input('Payment_Status_of_Previous_Credit', value=0)
+			acc_balance = st.selectbox(
+				'Account Balance',
+				['No Account', 'No Balance', 'Below 200DM', '200DM or Above'],
+				index=0,
+			)
+			credit_monthly = st.number_input('Duration of credit monthly', value=0)
+			payment_status = st.selectbox(
+				'Payment status',
+				['Delayed', 'Other Credits', 'Paid Up', 'No Problem with Current Credits', 'Previous Credits Paid'],
+				index=0,
+			)
 			purporse = st.number_input('Purpose', value=0)
 			credit_amount = st.number_input('Credit_Amount', value=0)
-			value_savings = st.number_input('Value_Savings_Stocks', value=0)
-			current_employment = st.number_input('Length_of_current_employment', value=0)
-			instalment_per_cent = st.number_input('Instalment_per_cent', value=0)
-			sex_martial = st.number_input('Sex_Marital_Status', value=0)
-			guarantors = st.number_input('Guarantors', value=0)
+			value_savings = st.selectbox(
+				'Saving/Stock value',
+				['None', 'Below 100DM', '100-500DM', '500-1000DM', 'Above 1000DM'],
+				index=0,
+			)
+			current_employment = st.selectbox(
+				'Length of current employment',
+				['Unemployed', '<1 Year', '1-4 Year', '4-7 Year', 'Above 7 Year'],
+				index=0,
+			)
+			instalment_per_cent = st.selectbox(
+				'Installments %',
+				['Above 35%', '25-35%', '20-25%', 'Below 20%'],
+				index=0,
+			)
+			sex_martial = st.selectbox(
+				'Sex and martial status',
+				['Male, Divorced', 'Male, Single', 'Male, Married/Widowed', 'Female'],
+				index=0,
+			)
+			guarantors = st.selectbox(
+				'Guarantor',
+				['None', 'Co-applicant', 'Guarantor'],
+				index=0,
+			)
 
 		with col_2:
-			duration_address = st.number_input('Duration_in_Current_address', value=0)
-			valuable_asset = st.number_input('M_valuable_available_asset', value=0)
+			duration_address = st.selectbox(
+				'Duration in current address',
+				['<1 Year', '1-4 Year', '4-7 Year', 'Above 7 Year'],
+				index=0,
+			)
+			valuable_asset = st.selectbox(
+				'Most valuable asset',
+				['None', 'Car', 'Life Insurance', 'Real Estate'],
+				index=0,
+			)
 			age = st.number_input('Age_years', value=0)
-			concurrent_credits = st.number_input('Concurrent_Credits', value=0)
-			apartment = st.number_input('Type_of_apartment', value=0)
-			credit_this_bank = st.number_input('No_of_Credits_at_this_Bank', value=0)
-			occupation = st.number_input('Occupation', value=0)
-			dependents = st.number_input('No_of_dependents', value=0)
-			telephone = st.number_input('Telephone', value=0)
-			foreign_worker = st.number_input('Foreign_Worker', value=0)
+			concurrent_credits = st.selectbox(
+				'Concurrent credits',
+				['Other Banks', 'Dept. Store', 'None'],
+				index=0,
+			)
+			apartment = st.selectbox(
+				'Type of apartment',
+				['Free', 'Rented', 'Owned'],
+				index=0,
+			)
+			credit_this_bank = st.selectbox(
+				'Number of credits at bank',
+				['1', '2 or 3', '4 or 5', 'Above 6'],
+				index=0,
+			)
+			occupation = st.selectbox(
+				'Occupation',
+				['Unemployed unskilled', 'Unskilled permanent resident', 'Skilled', 'Executive'],
+				index=0,
+			)
+			dependents = st.selectbox(
+				'Number of dependents',
+				['3 or More', 'Less than 3'],
+				index=0,
+			)
+			telephone = st.selectbox(
+				'Telephone',
+				['Yes', 'No'],
+				index=0,
+	 		)
+			foreign_worker = st.selectbox(
+				'Foreign worker',
+				['Yes', 'No'],
+				index=0,
+	 		)
 
 		submitted = st.form_submit_button('Submit')
 	if submitted:
 		df = [[
-			acc_balance, credit_monthly, payment_status, purporse, credit_amount, value_savings, current_employment, instalment_per_cent, sex_martial, guarantors, 
-			duration_address, valuable_asset, age, concurrent_credits, apartment, credit_this_bank, occupation, dependents, telephone, foreign_worker
+			['No Account', 'No Balance', 'Below 200DM', '200DM or Above'].index(acc_balance)+1,
+			credit_monthly,
+			['Delayed', 'Other Credits', 'Paid Up', 'No Problem with Current Credits', 'Previous Credits Paid'].index(payment_status)+1,
+			purporse, credit_amount,
+			['None', 'Below 100DM', '100-500DM', '500-1000DM', 'Above 1000DM'].index(value_savings)+1,
+			['Unemployed', '<1 Year', '1-4 Year', '4-7 Year', 'Above 7 Year'].index(current_employment)+1,
+			['Above 35%', '25-35%', '20-25%', 'Below 20%'].index(instalment_per_cent)+1,
+			['Male, Divorced', 'Male, Single', 'Male, Married/Widowed', 'Female'].index(sex_martial)+1,
+			['None', 'Co-applicant', 'Guarantor'].index(guarantors)+1, 
+			['<1 Year', '1-4 Year', '4-7 Year', 'Above 7 Year'].index(duration_address)+1,
+			['None', 'Car', 'Life Insurance', 'Real Estate'].index(valuable_asset)+1,
+			age, 
+			['Other Banks', 'Dept. Store', 'None'].index(concurrent_credits)+1,
+			['Free', 'Rented', 'Owned'].index(apartment)+1,
+			['1', '2 or 3', '4 or 5', 'Above 6'].index(credit_this_bank)+1,
+			['Unemployed unskilled', 'Unskilled permanent resident', 'Skilled', 'Executive'].index(occupation)+1,
+			['3 or More', 'Less than 3'].index(dependents)+1,
+			['Yes', 'No'].index(telephone)+1,
+			['Yes', 'No'].index(foreign_worker)+1
 		]]
 
 		return df
