4f950529ef2238fe44bbdfa089048d48e9dd8f68
rendysusanto40 <rendysusanto40@gmail.com>
2024-06-15 02:32:10 +0700

    add Data Preprocessing and Training Pages

--- pages/2_Prediction_Demo.py
+++ pages/2_Prediction_Demo.py
@@ -2,14 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-<<<<<<< HEAD
-<<<<<<< HEAD
 import time
-=======
->>>>>>> 1d3ec723eac47e244b6228e6d96167f4455457de
-=======
-import time
->>>>>>> 55984ea558587e1eb662bd78074fcf6f1c3ae5a7
 
 st.set_page_config(
 	page_title="Prediction Demo",
@@ -18,8 +11,6 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-<<<<<<< HEAD
-<<<<<<< HEAD
 	st.spinner('Loading model...')
 	with open('model/model.pickle', 'rb') as file:
 		model = pickle.load(file)
@@ -29,23 +20,6 @@ def load_data():
 	with open('scaler/standard_scaler.pickle', 'rb') as file:
 		scaler = pickle.load(file)
 		st.success('Scaler loaded!!')
-=======
-	load_state = st.text('Loading model...')
-=======
-	st.spinner('Loading model...')
->>>>>>> 55984ea558587e1eb662bd78074fcf6f1c3ae5a7
-	with open('model/model.pickle', 'rb') as file:
-		model = pickle.load(file)
-		st.success('Model loaded!!')
-	
-	st.spinner('Loading scaler...')
-	with open('scaler/standard_scaler.pickle', 'rb') as file:
-		scaler = pickle.load(file)
-<<<<<<< HEAD
->>>>>>> 1d3ec723eac47e244b6228e6d96167f4455457de
-=======
-		st.success('Scaler loaded!!')
->>>>>>> 55984ea558587e1eb662bd78074fcf6f1c3ae5a7
 	
 	return model, scaler
 
@@ -67,21 +41,11 @@ def form():
 				['Delayed', 'Other Credits', 'Paid Up', 'No Problem with Current Credits', 'Previous Credits Paid'],
 				index=0,
 			)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 55984ea558587e1eb662bd78074fcf6f1c3ae5a7
 			purpose = st.selectbox(
 				'Purpose',
 				['New Car', 'Used Car', 'Furniture', 'Radio/TV', 'Appliances', 'Repair', 'Vacation', 'Retraining', 'Business', 'Other'],
 				index=0,
 			)
-<<<<<<< HEAD
-=======
-			purporse = st.number_input('Purpose', value=0)
->>>>>>> 1d3ec723eac47e244b6228e6d96167f4455457de
-=======
->>>>>>> 55984ea558587e1eb662bd78074fcf6f1c3ae5a7
 			credit_amount = st.number_input('Credit Amount', value=0)
 			value_savings = st.selectbox(
 				'Saving/Stock value',
@@ -163,17 +127,8 @@ def form():
 			['No Account', 'No Balance', 'Below 200DM', '200DM or Above'].index(acc_balance)+1,
 			credit_monthly,
 			['Delayed', 'Other Credits', 'Paid Up', 'No Problem with Current Credits', 'Previous Credits Paid'].index(payment_status)+1,
-<<<<<<< HEAD
-<<<<<<< HEAD
 			['New Car', 'Used Car', 'Furniture', 'Radio/TV', 'Appliances', 'Repair', 'Vacation', 'Retraining', 'Business', 'Other'].index(purpose), 
 			credit_amount,
-=======
-			purporse, credit_amount,
->>>>>>> 1d3ec723eac47e244b6228e6d96167f4455457de
-=======
-			['New Car', 'Used Car', 'Furniture', 'Radio/TV', 'Appliances', 'Repair', 'Vacation', 'Retraining', 'Business', 'Other'].index(purpose), 
-			credit_amount,
->>>>>>> 55984ea558587e1eb662bd78074fcf6f1c3ae5a7
 			['None', 'Below 100DM', '100-500DM', '500-1000DM', 'Above 1000DM'].index(value_savings)+1,
 			['Unemployed', '<1 Year', '1-4 Year', '4-7 Year', 'Above 7 Year'].index(current_employment)+1,
 			['Above 35%', '25-35%', '20-25%', 'Below 20%'].index(instalment_per_cent)+1,
