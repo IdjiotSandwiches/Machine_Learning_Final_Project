613500d2d2a1eb83c29a89b39f4780c59eaef981
Vincen <vincenhayate123@gmail.com>
2024-06-15 11:44:21 +0700

    Change prep btn, pred demo selectbox, desc page table

--- pages/3_Data Preprocessing.py
+++ pages/3_Data Preprocessing.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 import streamlit as st
 from sklearn.utils import resample, shuffle
@@ -32,34 +33,31 @@ class DataPreprocessing:
 		self.df = shuffle(self.df)
   
 def data_preprocessing(file_path, null_handle, resample, shuffle):
-    data_prep = DataPreprocessing(file_path=file_path)
-    data_prep.load_data()
-    if null_handle == "Drop":
-        data_prep.drop_na()
-    else:
-        data_prep.mean_na()
-    if resample==True:
-        data_prep.resampling()
-    if shuffle==True:
-        data_prep.shuffle()
+   data_prep = DataPreprocessing(file_path=file_path)
+   data_prep.load_data()
+   if null_handle == "Drop":
+      data_prep.drop_na()
+   else:
+      data_prep.mean_na()
+   if resample==True:
+      data_prep.resampling()
+   if shuffle==True:
+      data_prep.shuffle()
 	
-    return data_prep
-
-
-
+   return data_prep
 
 def form_preprocessing():
-    with st.form("preprocessing"):
-        null_handle = st.selectbox("NA Handle", ["Drop", "Mean"], index=0)
-        resample = st.toggle("Resample")
-        shuffle = st.toggle("Shuffle")
-        if st.form_submit_button("Submit"):
-            dp = data_preprocessing("dataset/german.csv", null_handle, resample, shuffle)
-            st.success("Data Preprocessing Success")
-            st.session_state["data_preprocessing"] = dp
-            st.switch_page('pages/4_Training Model.py')
-
-    
+   with st.form("preprocessing"):
+      #   null_handle = st.selectbox("NA Handle", ["Drop", "Mean"], index=0)
+      null_handle = st.radio("NA Handle", ["Drop", "Mean"], index=0, horizontal=True)
+      resample = st.toggle("Resample")
+      shuffle = st.toggle("Shuffle")
+      if st.form_submit_button("Submit"):
+         dp = data_preprocessing("dataset/german.csv", null_handle, resample, shuffle)
+         st.success("Data Preprocessing Success")
+         st.session_state["data_preprocessing"] = dp
+         time.sleep(2)
+         st.switch_page('pages/4_Train_Your_Model.py')
 
 st.set_page_config(
 	page_title="Data Preprocessing",
