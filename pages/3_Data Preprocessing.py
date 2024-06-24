0a7bca7095f3e1e0e1f4b7bdbf203900c3d38215
rendysusanto40 <rendysusanto40@gmail.com>
2024-06-24 16:54:43 +0700

    Put split and scaler in data preprocessing page

--- pages/3_Data Preprocessing.py
+++ pages/3_Data Preprocessing.py
@@ -1,12 +1,18 @@
 import time
 import pandas as pd
 import streamlit as st
+import pickle
 from sklearn.utils import resample, shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 class DataPreprocessing:
 	def __init__(self, file_path):
 		self.file_path = file_path
 		self.df = None
+		self.X = None
+		self.Y = None
+		self.x_train, self.x_test, self.y_train, self.y_test = [None] * 4
 		
 	def load_data(self):
 		self.df = pd.read_csv(self.file_path, sep=';')
@@ -32,32 +38,65 @@ class DataPreprocessing:
 	def shuffle(self):
 		self.df = shuffle(self.df)
   
-def data_preprocessing(file_path, null_handle, resample, shuffle):
-   data_prep = DataPreprocessing(file_path=file_path)
-   data_prep.load_data()
-   if null_handle == "Drop":
-      data_prep.drop_na()
-   else:
-      data_prep.mean_na()
-   if resample==True:
-      data_prep.resampling()
-   if shuffle==True:
-      data_prep.shuffle()
+	def seperate_data(self):
+		self.X = self.df.drop('Creditability', axis=1)
+		self.Y = self.df['Creditability']
+  
+	def split_data(self, test_size=0.2, random_state=42):
+		self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.X, self.Y, test_size=test_size, random_state=random_state
+        )
+  
+	def Scaler(self, scaler, scaler_name):
+		self.x_train = scaler.fit_transform(self.x_train)
+		self.x_test = scaler.transform(self.x_test)
+		self.scaler = scaler
+		self.save_scaler(f'scaler/user_trained/{scaler_name}.pickle')
+  
+	def save_scaler(self, path):
+		with open(path, 'wb') as file:
+			pickle.dump(self.scaler, file)
+  
+def data_preprocessing(file_path, null_handle, resample, shuffle, test_size, scaler, scaler_name):
+	data_prep = DataPreprocessing(file_path=file_path)
+	data_prep.load_data()
+	if null_handle == "Drop":
+		data_prep.drop_na()
+	else:
+		data_prep.mean_na()
+	if resample==True:
+		data_prep.resampling()
+	if shuffle==True:
+		data_prep.shuffle()
+	data_prep.seperate_data()
+	data_prep.split_data(test_size)
+	data_prep.Scaler(scaler, scaler_name)
 	
-   return data_prep
+	return data_prep
 
 def form_preprocessing():
-   with st.form("preprocessing"):
-      #   null_handle = st.selectbox("NA Handle", ["Drop", "Mean"], index=0)
-      null_handle = st.radio("NA Handle", ["Drop", "Mean"], index=0, horizontal=True)
-      resample = st.toggle("Resample")
-      shuffle = st.toggle("Shuffle")
-      if st.form_submit_button("Submit"):
-         dp = data_preprocessing("dataset/german.csv", null_handle, resample, shuffle)
-         st.success("Data Preprocessing Success")
-         st.session_state["data_preprocessing"] = dp
-         time.sleep(1)
-         st.switch_page('pages/4_Train_Your_Model.py')
+	with st.form("preprocessing"):
+		test_size = st.slider(label="Test Size",  min_value=0.10, max_value=0.90, format="%.2f")
+		null_handle = st.radio("NA Handle", ["Drop", "Mean"], index=0, horizontal=True)
+		col1, col2 = st.columns(2)
+		with col1:
+			resample = st.toggle("Resample")
+		with col2:
+			shuffle = st.toggle("Shuffle")
+		scaler = st.selectbox("Scaler", options=["StandardScaler", "MinMaxScaler"], index=0)
+		scaler_name = st.text_input("Enter your scaler name", value='user_scaler')
+
+    
+		if st.form_submit_button("Submit"):
+			if scaler == "StandardScaler":
+				scaler = StandardScaler()
+			else:
+				scaler = MinMaxScaler()
+			dp = data_preprocessing("dataset/german.csv", null_handle, resample, shuffle, test_size, scaler, scaler_name)
+			st.success("Data Preprocessing Success")
+			st.session_state["data_preprocessing"] = dp
+			time.sleep(1)
+			st.switch_page('pages/4_Train_Your_Model.py')
 
 st.set_page_config(
 	page_title="Data Preprocessing",
--- pages/4_Train_Your_Model.py
+++ pages/4_Train_Your_Model.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
@@ -15,41 +13,18 @@ st.set_page_config(
 )
 
 class Model:
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, dp):
+        self.dp = dp
         self.X = None
         self.Y = None
         self.y_predict = None
-        self.scaler = None
-        self.x_train, self.x_test, self.y_train, self.y_test = [None] * 4
-    
-    # Seperate between X and Y
-    def seperate_data(self):
-        self.X = self.df.drop('Creditability', axis=1)
-        self.Y = self.df['Creditability']
-
-    # Train Test Split
-    def split_data(self, test_size=0.2, random_state=42):
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.Y, test_size=test_size, random_state=random_state
-        )
-
-    def Scaler(self, scaler, scaler_name):
-        self.x_train = scaler.fit_transform(self.x_train)
-        self.x_test = scaler.transform(self.x_test)
-        self.scaler = scaler
-        self.save_scaler(self.scaler, f'scaler/user_trained/{scaler_name}.pickle')
-
-    def save_scaler(self, scaler, path):
-        with open(path, 'wb') as file:
-            pickle.dump(scaler, file)
     
     # Model creation
     def create_model(self, modelused):
         self.model = modelused
 
     def train_model(self, model_name):
-        self.model.fit(self.x_train, self.y_train)
+        self.model.fit(self.dp.x_train, self.dp.y_train)
         self.save_model(self.model, f'model/user_trained/{model_name}.pickle')
     
     def save_model(self, model, path):
@@ -57,53 +32,44 @@ class Model:
             pickle.dump(model, file)
     
     def model_predict(self):
-        self.y_predict = self.model.predict(self.x_test)
+        self.y_predict = self.model.predict(self.dp.x_test)
 
     def predict(self, input):
-        if self.scaler:
-            input = self.scaler.transform(input)
+        input = self.dp.scaler.transform(input)
         return self.model.predict(input)
 
     # Evaluation
     def report(self):
-        cm = confusion_matrix(self.y_test, self.y_predict, labels=self.model.classes_)
+        st.subheader(f"Accuracy: {round(accuracy_score(self.dp.y_test, self.y_predict) * 100, 2)}%")
+        st.subheader("Confusion Matrix:")
+        cm = confusion_matrix(self.dp.y_test, self.y_predict, labels=self.model.classes_)
         # disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=self.model.classes_)
         # disp.plot(cmap="Blues")
         fig = px.imshow(cm, text_auto=True)
         st.plotly_chart(fig, theme="streamlit")
-        st.write(f"Accuracy: {round(accuracy_score(self.y_test, self.y_predict) * 100, 2)}%")
+        
 
 def form_model():
-    test_size = st.slider(label="Test Size",  min_value=0.10, max_value=0.90, format="%.2f")
     select_model = st.selectbox(label="Select Model", options=["RandomForestClassifer", "SVC", "LogisticRegression", "AdaBoostClassifier"])
-    scaler = st.selectbox("Scaler", options=["StandardScaler", "MinMaxScaler"], index=0)
 
     if select_model == "RandomForestClassifer":
-        randomforestform(Model(st.session_state["data_preprocessing"].df), scaler=scaler, test_size=test_size)
+        randomforestform(Model(st.session_state["data_preprocessing"]))
     elif select_model  == "SVC":
-        svcform(Model(st.session_state["data_preprocessing"].df), scaler=scaler, test_size=test_size)
+        svcform(Model(st.session_state["data_preprocessing"]))
     elif select_model  == "LogisticRegression":
-        logisticform(Model(st.session_state["data_preprocessing"].df), scaler=scaler, test_size=test_size)
+        logisticform(Model(st.session_state["data_preprocessing"]))
     elif select_model  == "AdaBoostClassifier":
-        adaform(Model(st.session_state["data_preprocessing"].df), scaler=scaler, test_size=test_size)
+        adaform(Model(st.session_state["data_preprocessing"]))
 
-def randomforestform(model, scaler, test_size):
+def randomforestform(model):
     with st.form("RandomForestClassifer Parameter"):
         n_est = st.number_input('Enter the number of estimators: ', min_value=0, value=100)
         m_dep = st.number_input('Enter the max depth: ', min_value=0, value=20)
         crit = st.selectbox('Enter the criterion',  ["gini", "entropy","log_loss"], index=0)
         c_wght = st.selectbox('Enter the class weight', ["balanced", "balanced_subsample"], index=0)
         model_name = st.text_input("Enter your model name", value='user_model')
-        scaler_name = st.text_input("Enter your scaler name", value='user_scaler')
         if st.form_submit_button("Submit"):
             chosen_model = RandomForestClassifier(n_estimators=n_est, max_depth=m_dep, criterion=crit, class_weight=c_wght)
-            model.seperate_data()
-            model.split_data(test_size=test_size)
-            if scaler == "StandardScaler":
-                chosen_scaler = StandardScaler()
-            else:
-                chosen_scaler = MinMaxScaler()
-            model.Scaler(chosen_scaler, scaler_name)
             model.create_model(chosen_model)
             model.train_model(model_name)
             model.model_predict()
@@ -111,62 +77,38 @@ def randomforestform(model, scaler, test_size):
             st.session_state["model"] = model
             st.success("Model Train Successful")   
 
-def svcform(model, scaler, test_size):
+def svcform(model):
     with st.form("SVC Parameter"):
         kernels = st.selectbox("Enter the kernel choice", ["linear", "poly", "rbf", "sigmoid"], index=0)
         model_name = st.text_input("Enter your model name", value='user_model')
-        scaler_name = st.text_input("Enter your scaler name", value='user_scaler')
         if st.form_submit_button("Submit"):
             chosen_model = SVC(kernel=kernels)
-            model.seperate_data()
-            model.split_data(test_size=test_size)
-            if scaler == "StandardScaler":
-                chosen_scaler = StandardScaler()
-            else:
-                chosen_scaler = MinMaxScaler()
-            model.Scaler(chosen_scaler, scaler_name)
             model.create_model(chosen_model)
             model.train_model(model_name)
             model.model_predict()
             model.report()
             st.session_state["model"] = model
 
-def logisticform(model, scaler, test_size):
+def logisticform(model):
     with st.form("Logistic Regression Parameter"):
         c = st.number_input('Enter the regularization parameter (C): ', min_value= 1.00, format="%.2f")
         solvers = st.selectbox('Enter the solver: ', ['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga'], index=0)
         model_name = st.text_input("Enter your model name", value='user_model')
-        scaler_name = st.text_input("Enter your scaler name", value='user_scaler')
         if st.form_submit_button("Submit"):
             chosen_model = LogisticRegression(C=c, solver=solvers)
-            model.seperate_data()
-            model.split_data(test_size=test_size)
-            if scaler == "StandardScaler":
-                chosen_scaler = StandardScaler()
-            else:
-                chosen_scaler = MinMaxScaler()
-            model.Scaler(chosen_scaler, scaler_name)
             model.create_model(chosen_model)
             model.train_model(model_name)
             model.model_predict()
             model.report()
             st.session_state["model"] = model
 
-def adaform(model, scaler, test_size):
-    with st.form("LOgistic Regression Parameter"):
+def adaform(model):
+    with st.form("Ada Boost Parameter"):
         n_est = st.number_input('Enter the number of estimators: ', min_value=0, value=100)
         l_rate = st.number_input('Enter the learning rate: ', min_value= 0.01, value= 0.05, format="%.2f")
         model_name = st.text_input("Enter your model name", value='user_model')
-        scaler_name = st.text_input("Enter your scaler name", value='user_scaler')
         if st.form_submit_button("Submit"):
             chosen_model = AdaBoostClassifier(n_estimators=n_est, learning_rate=l_rate)
-            model.seperate_data()
-            model.split_data(test_size=test_size)
-            if scaler == "StandardScaler":
-                chosen_scaler = StandardScaler()
-            else:
-                chosen_scaler = MinMaxScaler()
-            model.Scaler(chosen_scaler, scaler_name)
             model.create_model(chosen_model)
             model.train_model(model_name)
             model.model_predict()
